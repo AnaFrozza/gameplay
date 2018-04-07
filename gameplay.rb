f15e2ad1d0dd334822a89f712c806e191a48b7d7
dev3@numerouno.com.br <dev3@numerouno.com.br>
2018-04-07 04:01:18 -0300

    done

--- gameplay.rb
+++ gameplay.rb
@@ -6,45 +6,178 @@ class Newgame
 
   	
   	def registroCena(cena, id)
+  		itens = []
+  		i = 1
+
   		titulo = [cena[1][1]]
   		descricao = [cena[2][1]]
   		pecas = cena[3]
-  		itens = []
-  		i = 1
+  		
   		while pecas[i] != nil do
   			itens[i] = pecas[i]
   			i += 1
   		end
+
+
   		puts titulo
   		puts descricao
-
+  		
+  		return itens[1]
 
   		#motor(titulo, descricao)
   	end
 
-  	def motor(mundo, id, objetos)
-  		while id < 2#mundo.size
-  			puts registroCena(mundo[id], id)
+	#Seracao dos comandos para verificacao
+  	def confirma_comando_use(comando)
+  		lista_comando = 0
+  		verificacao_comando = []
 
-  			if objetos[1][8][1] == '0'
-  				id += 1	
-  			else
-  				puts "Cena ainda nao concluida"
-  				break
-  			end
+		while lista_comando <= comando[1].size
+			verificacao_comando = comando[1].to_a
+			if verificacao_comando[lista_comando][0] == "u"
+				return 0 	
+			end 
+			lista_comando += 1
+		end
 
+		return 1
 
-  			 	
-  		end 
   	end
 
-  	def registroInventario
-  		mochila = []
+  	def confirma_comando_get(comando)
+  		lista_comando = 0
+  		verificacao_comando = []
+  		
+		while lista_comando <= comando[1].size
+			verificacao_comando = comando[1].to_a
+			if verificacao_comando[lista_comando][0] == "g"
+				return 0 	
+			end 
+			lista_comando += 1
+		end
+
+		return 1
+
+  	end
+
+  	def confirma_comando_check(comando)
+  		lista_comando = 0
+  		verificacao_comando = []
+  		
+		while lista_comando <= comando[1].size
+			verificacao_comando = comando[1].to_a
+			if verificacao_comando[lista_comando][0] == "c"
+				return 0 	
+			end 
+			lista_comando += 1
+		end
+
+		return 1
+
+  	end
+
+  	def motor(mundo, id, objetos, mochila, inventario)
+
+  		while id < mundo.size
+  			comado_terminal = []
+  			texto_comando = []
+  			comando_comando = []
+  			objeto_comando = []
+  			verificacao_comando = []
+  			pecas = 0
+  			itens = 0
+  			lista_comando = 0
+
+  			#chama a cena_atual
+  			string_objeto_cena = registroCena(mundo[id], id)
+  			
+			#Acao do usuario
+			comado_terminal = gets
+
+			texto_comando = comado_terminal.to_s
+			comando_comando = texto_comando.scan(/\w+/)[0]
+			objeto_comando = texto_comando.scan(/\w+/)[1]
+
+
+
+			#Escobrindo o objeto digitado
+			while objeto_comando != objetos[string_objeto_cena[pecas]][2][1] 
+				pecas += 1
+			end	
+			
+			#Controle do objeto
+			id_objeto = objetos[string_objeto_cena[pecas]][0][1]
+			descricao_objeto = objetos[string_objeto_cena[pecas]][3]
+			res_pos = objetos[string_objeto_cena[pecas]][4][1]
+			res_neg = objetos[string_objeto_cena[pecas]][5]
+			comando = objetos[string_objeto_cena[pecas]][6]
+			cena_alvo = objetos[string_objeto_cena[pecas]][7][1] 
+			
+			
+			#Controle do comando
+			if comando_comando == "use"
+				tem_comando = confirma_comando_use(comando)
+				if tem_comando == 0
+					puts "da pra usar"
+				else
+					"Execucao incorreta"
+				end 
+
+
+			elsif comando_comando == "get"
+				tem_comando = confirma_comando_get(comando)
+				
+				if tem_comando == 0
+					ver_iten = 0
+
+					while ver_iten <= mochila.size
+						if mochila[ver_iten] == id_objeto
+							puts "Item ja se encontra no inventario"
+						end
+						ver_iten += 1
+					end
+					if ver_iten > mochila.size
+						mochila[inventario] = id_objeto;
+						puts res_pos
+						inventario += 1
+					end
+
+					if cena_alvo != 0
+						id = cena_alvo
+						motor(mundo, id, objetos, mochila, inventario)
+					end
+
+				else
+					"Execucao incorreta"
+				end 
+				
+			elsif comando_comando == "check"	
+				tem_comando = confirma_comando_check(comando)
+				if tem_comando == 0
+					puts "da pra usar"
+				else
+					"Execucao incorreta"
+				end 
+			else
+				puts "comando nao encontrado"
+			end
+			
+  		end 
+
+  		puts "Parabens,"
+  		puts "você salvou a Pedra Filosofal."
+  	end
+
+  	def registroInventario(id)
+  		
   	end
 
 	def mainMenu(iCena)
 		mundo = []
 		objetos = []
+		mochila = []
+  		itens_inventario = 0
+		
 
 		id = iCena
 		iObjeto = 1
@@ -65,7 +198,6 @@ class Newgame
 			i.to_s
 	  		cena_atual = i[1].to_i	  		
 	  		mundo[cena_atual] = cena
-	  		
 
 	  		#encremento das cenas
 	  		iCena += 1
@@ -94,7 +226,7 @@ class Newgame
 			
 		end
 
-		motor(mundo, id, objetos)
+		motor(mundo, id, objetos, mochila, itens_inventario)
 		
 		
 
